f5a2879695a17d6237bbbdbf18e796c760178a9d
Rasedur Rahman Roxy <roxy.xprez@gmail.com>
2022-04-11 05:52:41 +0600

    Removed storybook conf from libs

--- apps/groundups-react-e2e/src/integration/groundups-react/groundups-react.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('groundups-react: GroundupsReact component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=groundupsreact--primary'));
-    
-    it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to GroundupsReact!');
-    });
-});
